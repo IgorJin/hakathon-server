f9f13284961b422c91c4cd914ec920f0044d59a8
igor.z <igor.z@fora-soft.com>
2021-05-25 22:32:09 +0300

    response

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('from git');
+  res.send('from dev git');
 });
 
 module.exports = router;
