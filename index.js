94faccc76ad948f899e07a3378eb2a68f9c4906d
igor.z <igor.z@fora-soft.com>
2021-06-05 11:05:32 +0300

    ADD HEALT ROUTE

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", (req, res) => res.status(200).send('SERVER IS ALIVE'))
+
 /*
 //
   ROTES
